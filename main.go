626763f6d75062affb812e073dca65ad00ffb94a
Luke Maciak <luke@maciak.net>
2026-01-21 00:17:48 -0500

    Added support for http proxies.
    Fixes #1

--- main.go
+++ main.go
@@ -6,19 +6,24 @@ import (
     "fmt"
     "flag"
     "bytes"
+    "net/url"
     "net/http"
     "mime/multipart"
     "path/filepath"
 )
 
-const version = "0.1.0"
+const version = "0.1.1"
 const useragent = "Go-http-client/1.1/ (luke at maciak.net) post/" + version
 
 func main() {
 
-    var url string
-    flag.StringVar(&url, "url", "https://0x0.st", "URL to upload file to")
-    flag.StringVar(&url, "u", "https://0x0.st", "URL to upload file to")
+    var urlFlag string
+    flag.StringVar(&urlFlag, "url", "https://0x0.st", "URL to upload file to")
+    flag.StringVar(&urlFlag, "u", "https://0x0.st", "URL to upload file to")
+
+    var proxy string
+    flag.StringVar(&proxy, "proxy", "", "HTTP proxy to use")
+    flag.StringVar(&proxy, "p", "", "HTTP proxy to use")
 
     var ver bool
     flag.BoolVar(&ver, "version", false, "show version")
@@ -41,7 +46,7 @@ func main() {
             Usage()
 
         default:
-            Post(url, args[0])
+            Post(urlFlag, args[0], proxy)
         } 
     } else {
         Usage()
@@ -59,13 +64,17 @@ func Usage() {
     fmt.Println("  -v, --version    Print version information")
     fmt.Println("  -h, --help       Print this message and exit")
     fmt.Println("  -u, --url        URL to upload file to (default: https://0x0.st)")
+    fmt.Println("  -p, --proxy      HTTP proxy to use (eg. http://user:pass@host:port)")
     os.Exit(0)
 }
 
-func Post(url string, file string) {
+func Post(urlPost string, file string, proxy string) {
 
-    fmt.Println("\nURL:", url)
+    fmt.Println("\nURL:", urlPost)
     fmt.Println("File:", file)
+    if proxy != "" {
+        fmt.Println("Proxy:", proxy)
+    }
 
     // open file
     file_reader, err := os.Open(file)
@@ -100,7 +109,7 @@ func Post(url string, file string) {
     writer.Close()
 
     // create a new request
-    req, err := http.NewRequest("POST", url, &buff)
+    req, err := http.NewRequest("POST", urlPost, &buff)
     if err != nil {
         fmt.Fprintln(os.Stderr, "Error creating HTTP request:", err)
         os.Exit(1)
@@ -111,7 +120,22 @@ func Post(url string, file string) {
     req.Header.Set("Content-Type", writer.FormDataContentType())
     req.Header.Set("User-Agent", useragent)
 
+    // create a new http client
     client := &http.Client{}
+
+    // if a proxy is specified, create a new transport and set the proxy
+    if proxy != "" {
+        proxyUrl, err := url.Parse(proxy)
+        if err != nil {
+            fmt.Fprintln(os.Stderr, "Error parsing proxy URL:", err)
+            os.Exit(1)
+        }
+        transport := &http.Transport{
+            Proxy: http.ProxyURL(proxyUrl),
+        }
+        client.Transport = transport
+    }
+
     resp, err := client.Do(req)
     if err != nil {
         fmt.Fprintln(os.Stderr, "Error sending the HTTP request:", err)
@@ -123,6 +147,9 @@ func Post(url string, file string) {
     msg := string(response)
 
     fmt.Println("Status:", resp.Status)
+    for k, v := range resp.Header {
+        fmt.Printf("%s: %s\n", k, v)
+    }
     fmt.Println("Response:", msg)
 
 }
